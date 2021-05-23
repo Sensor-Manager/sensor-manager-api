a5343db6a292e474a6f311a274c945573d3ee3fb
Calvin Dsouza <calvin091518dsouza@gmail.com>
2021-05-23 16:24:23 +0530

    testing api

--- app.py
+++ app.py
@@ -1,38 +1,54 @@
-from re import DEBUG
 from flask import Flask, json,request,jsonify
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
 
-devices=[
-    {'id': 0,
-     'name': 'GM IoT',
-     'temperature': '30°C',
-     'humidity': '84%',
-     },
-]
+# devices=[
+#     {'id': 0,
+#      'name': 'GM IoT',
+#      'temperature': '30°C',
+#      'humidity': '84%',
+#      },
+# ]
 
 @app.route("/")
 def home():
     return "Welcome to Grain Manager API"
 
+
 @app.route("/devices",methods=["GET"])
 def getDevices():
+    f=open("data.json","r")
+    devices = json.loads(f.read())
+    f.close()
+    print(jsonify(devices))
     return jsonify(devices)
 
 @app.route("/editdata/<id>",methods=["POST"])
 def editDeviceData(id):
     input_json = request.get_json(force=True)
-
+    f=open("data.json","r")
+    devices = json.loads(f.read())
+    f.close()
     for device in devices:
         if int(id) == device["id"]:
             device["name"]=input_json['name']
             device["temperature"]=input_json['temperature']
             device["humidity"]=input_json['humidity']
-            return jsonify(device)
-
+    f=open("data.json","w")
+    jsonString= json.dumps(devices)
+    f.write(jsonString)
+    f.close()
     dictToReturn = {'editSuccessful':True}
     return jsonify(dictToReturn)
+
+@app.route('/test',methods=['POST'])
+def testing():
+    input_json = request.get_json(force=True)
+    print(type(input_json))
+    dictToReturn = {'editSuccessful':True}
+    return jsonify(input_json)
 
 if __name__ == '__main__':
     app.run(threaded=True,port=8000)
